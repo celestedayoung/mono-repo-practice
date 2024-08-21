65959fe0966a16e1fd193e3a7fb2841618f9f54e
celestedayoung <dayoungpyo@gmail.com>
2024-08-21 18:47:39 +0900

    init

--- sos/packages/components/react/src/list/message/MessageList.tsx
+++ sos/packages/components/react/src/list/message/MessageList.tsx
@@ -13,7 +13,7 @@ interface MessageListProps extends Omit<CommonProps, 'color'> {
   content: string;
   isRead: boolean;
   isChecked: boolean;
-  following: string;
+  following?: string;
   variant: 'default' | 'danger' | 'warning';
   mode: 'default' | 'round';
 }
