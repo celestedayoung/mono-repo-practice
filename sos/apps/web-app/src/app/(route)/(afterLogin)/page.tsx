2465af55be07cea70441c02aa68832e4d5b51b8f
dayoungpyo <144453750+celestedayoung@users.noreply.github.com>
2024-08-21 18:58:48 +0900

    Update page.tsx

--- sos/apps/web-app/src/app/(route)/(afterLogin)/page.tsx
+++ sos/apps/web-app/src/app/(route)/(afterLogin)/page.tsx
@@ -1,106 +1,200 @@
-import { forwardRef } from 'react';
-import { CommonProps } from '../../common/types';
-import { Container } from '../../common/container/Container';
-import { Text } from '../../common/text/Text';
-import { Badge } from '../../badge/Badge';
-import { Icon } from '../../common/icon/Icon';
-import { messageListRecipe } from './style.css';
-import clsx from 'clsx';
+'use client';
 
-interface MessageListProps extends Omit<CommonProps, 'color'> {
-  title?: string;
-  date: string;
-  content: string;
-  isRead: boolean;
-  isChecked: boolean;
-  following?: string | undefined;
-  variant: 'default' | 'danger' | 'warning';
-  mode: 'default' | 'round';
+import React, { useState, useEffect } from 'react';
+import {
+  Notification,
+  InfoButton,
+  Container,
+  WeatherBox,
+  Headline,
+  MessageList,
+  MoreButton,
+} from '@sos/components-react';
+import { useFetchUserDisasters, useFetchFollowings, useFetchWeather } from '@/app/_hooks';
+import { useRouter } from 'next/navigation';
+import Link from 'next/link';
+
+declare global {
+  interface Window {
+    ReactNativeWebView?: {
+      postMessage(message: string): void;
+    };
+  }
 }
 
-const colorMap: { [key in MessageListProps['variant']]: 'blue' | 'pink' | 'orange' } = {
-  default: 'blue',
-  danger: 'pink',
-  warning: 'orange',
-};
+export default function Home() {
+  const router = useRouter();
 
-/**
- * MessageList 컴포넌트
- *
- * @param {string} props.title - 메시지 제목 (선택)
- * @param {Date} props.date - 메시지 날짜 (필수)
- * @param {string} props.content - 메시지 내용 (필수)
- * @param {boolean} props.isRead - 읽음 여부 (필수)
- * @param {boolean} props.isChecked - 체크 여부 (필수)
- * @param {string} props.following - 팔로잉 상태 (필수)
- * @param {'default' | 'danger' | 'warning'} props.variant - 메시지 타입 (필수)
- * @param {'default' | 'round'} props.mode - 메시지 모드 (필수)
- * @param {string} [props.className] - 추가 CSS 클래스 (선택)
- * @param {...any} props - 기타 HTML 속성
- * @param {React.Ref<HTMLElement>} ref - 전달받은 ref
- */
-export const MessageList = forwardRef<HTMLElement, MessageListProps>(
-  (
-    {
-      title,
-      date = '2024/07/18 09:15:40',
-      content = 'content',
-      isRead = false,
-      isChecked = false,
-      following = 'following',
-      variant,
-      mode = 'default',
-      className,
-      ...props
-    },
-    ref,
-  ) => {
-    const color = colorMap[variant] || 'blue';
-    const messageListClass = messageListRecipe({ variant, mode });
+  const routeToPage = (route: string) => {
+    if (typeof window !== 'undefined' && window.ReactNativeWebView) {
+      window.ReactNativeWebView.postMessage(
+        JSON.stringify({
+          type: 'NAVIGATE',
+          url: route,
+        }),
+      );
+    } else {
+      router.push(route, undefined);
+    }
+  };
 
-    return (
-      <Container
-        ref={ref}
-        as="li"
-        className={clsx(messageListClass, className)}
-        display="flex"
-        alignItems="center"
-        paddingX={16}
-        paddingY={12}
-        style={{ cursor: 'pointer' }}
-        {...props}
-      >
-        {!isRead && (
-          <Container height="100%" display="flex" paddingRight={12}>
-            <Icon size={8} color={`${color}500`}>
-              circle
-            </Icon>
-          </Container>
-        )}
-        <Container display="flex" flexDirection="column" width="100%" gap={8}>
-          <Container display="flex" flexDirection="column" gap={2}>
-            <Container display="flex" alignItems="center" height={24} gap={8}>
-              {title && (
-                <Text as="h5" textType="body2" textMode="bold">
-                  {title}
-                </Text>
-              )}
-              <Badge color={color}>{following}</Badge>
-              {isChecked && (
-                <Icon size={20} color={`${color}500`}>
-                  verified
-                </Icon>
-              )}
-            </Container>
-            <Text textType="label" color="textAssistive">
-              {date}
-            </Text>
-          </Container>
-          <Text textType="body3" textMode="reading">
-            {content}
-          </Text>
+  const handleLinkClick = (e: React.MouseEvent<HTMLAnchorElement>, href: string) => {
+    e.preventDefault();
+    routeToPage(href);
+  };
+
+  const { data } = useFetchUserDisasters();
+  const disasterResponses = data?.disasterResponses ?? [];
+
+  const weatherInfo = useFetchWeather(37.5665, 126.978).data;
+  const followingMembers = useFetchFollowings().data?.followResponses ?? [];
+
+  type AlertLevel = 'default' | 'danger' | 'warning' | 'safe' | 'ghost' | undefined;
+  type AlertLevels = { [key: string]: AlertLevel };
+
+  const [alertLevels, setAlertLevels] = useState<AlertLevels>({});
+
+  useEffect(() => {
+    const initialAlertLevels: AlertLevels = followingMembers.reduce((acc: AlertLevels, member) => {
+      const level = getInitialAlertLevel(member.nickName);
+      acc[member.nickName] = level;
+      return acc;
+    }, {});
+
+    if (JSON.stringify(alertLevels) !== JSON.stringify(initialAlertLevels)) {
+      setAlertLevels(initialAlertLevels);
+    }
+  }, [followingMembers, disasterResponses]);
+
+  function getInitialAlertLevel(memberNickName: string): AlertLevel {
+    const memberResponse = disasterResponses.find(
+      (response) => response.followName === memberNickName,
+    );
+    if (memberResponse) {
+      if (memberResponse.level === '위급재난') return 'danger';
+      if (memberResponse.level === '긴급재난') return 'warning';
+    }
+    return 'default';
+  }
+
+  const handleButtonClick = (memberNickName: string) => {
+    if (alertLevels[memberNickName] === 'danger') {
+      setAlertLevels((prevLevels) => ({
+        ...prevLevels,
+        [memberNickName]: 'safe',
+      }));
+    }
+  };
+
+  const latestCriticalResponse = disasterResponses
+    .filter((response) => response.level === '위급재난' || response.level === '긴급재난')
+    .sort((a, b) => new Date(b.sendTime).getTime() - new Date(a.sendTime).getTime())[0];
+
+  return (
+    <div
+      style={{ display: 'flex', flexDirection: 'column', minHeight: '100vh', overflowY: 'auto' }}
+    >
+      {latestCriticalResponse && (
+        <Container paddingX={16} paddingY={12}>
+          <Notification
+            state={latestCriticalResponse.level === '위급재난' ? 'danger' : 'warning'}
+            content={`[태풍] ${latestCriticalResponse.locationName}: 행동강령 확인`}
+          />
+        </Container>
+      )}
+      <Container paddingX={16}>
+        <WeatherBox
+          temperature={weatherInfo?.temp ?? '-'}
+          highestTemperature="30"
+          lowestTemperature="28"
+          iconSrc="/rainy.png"
+          windSpeed={weatherInfo?.windSpeed ?? '-'}
+          condition=""
+          date={new Date().toLocaleDateString('ko-KR', {
+            year: 'numeric',
+            month: 'long',
+            day: 'numeric',
+            weekday: 'long',
+          })}
+          location="서울특별시 강남구"
+        />
+      </Container>
+      <Container display="flex" flexDirection="column" paddingY={16}>
+        <Container display="flex" justifyContent="space-between">
+          <Headline>친구 현황</Headline>
+        </Container>
+        <Container display="flex" paddingX={12} paddingY={8} gap={16}>
+          {followingMembers.map((member, index) => (
+            <InfoButton
+              key={index}
+              variant={
+                member.nickName === '엄마'
+                  ? 'danger'
+                  : member.nickName === '동생'
+                    ? 'warning'
+                    : 'default'
+              }
+              badgeText={
+                member.nickName === '엄마'
+                  ? '위급재난'
+                  : member.nickName === '동생'
+                    ? '긴급재난'
+                    : ''
+              }
+              subText={alertLevels[member.nickName] === 'safe' ? '안전' : ''}
+              size="s"
+              onClick={() => {
+                const memberResponse = disasterResponses.find(
+                  (response) => response.followName === member.nickName,
+                );
+                if (
+                  alertLevels[member.nickName] === 'danger' &&
+                  memberResponse &&
+                  !memberResponse.mine
+                ) {
+                  handleButtonClick(member.nickName);
+                }
+              }}
+            >
+              {member.nickName}
+            </InfoButton>
+          ))}
         </Container>
       </Container>
-    );
-  },
-);
+      <Container display="flex" flexDirection="column" paddingY={16}>
+        <Container display="flex" justifyContent="space-between">
+          <Headline>재난 문자</Headline>
+          <Link href={'/emergency-alert'} onClick={(e) => handleLinkClick(e, '/emergency-alert')}>
+            <MoreButton />
+          </Link>
+        </Container>
+        <Container>
+          <Container display="flex" flexDirection="column" paddingX={16} paddingY={8} gap={8}>
+            {disasterResponses.map((response) => (
+              <MessageList
+                key={response.id}
+                mode="round"
+                isRead={true}
+                isChecked={false}
+                {...(response.mine ? {} : { following: response.followName })}
+                variant={
+                  response.level === '위급재난'
+                    ? 'danger'
+                    : response.level === '긴급재난'
+                      ? 'warning'
+                      : 'default'
+                }
+                title={`[${response.classification}] ${response.locationName}`}
+                content={response.msg}
+                date={new Date(response.sendTime).toLocaleString()}
+                onClick={() => {
+                  routeToPage(`/emergency-alert/${response.id}`);
+                }}
+              />
+            ))}
+          </Container>
+        </Container>
+      </Container>
+    </div>
+  );
+}
